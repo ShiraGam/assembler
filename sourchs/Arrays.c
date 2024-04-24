320d23c5b11d07a0f6509c8323278817b8dea831
ShiraGam <150137422+ShiraGam@users.noreply.github.com>
2024-04-24 15:16:19 +0300

    Create Arrays.c

---
+++ sourchs/Arrays.c
@@ -0,0 +1,25 @@
+
+#include "Arrays.h"
+#include "Global_def.h"
+
+void set_bit(bit_field *bitfield, int pos, int value) {
+    if (value)
+        bitfield->data |= (1 << pos);
+    else
+        bitfield->data &= ~(1 << pos);
+}
+
+void print_bits(bit_field *array, int size) {
+	int i = 0,j;
+    for (; i < size; ++i) {
+        unsigned int bits = array[i].data;
+        for ( j = BIT_FIELD_SIZE - 1; j >= 0; --j) {
+            printf("%d", (bits >> j) & 1);
+        }
+        printf("\n");
+    }
+    printf("\n\n\n");
+}
+
+
+
